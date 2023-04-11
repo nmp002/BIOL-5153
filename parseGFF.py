c227532f426b706a0df90cb84445ebcc9535e9fd
Nicholas Powell <nmp002@uark.edu>
2023-04-11 13:56:56 -0500

    first commit; part 1 of assn6

---
+++ parseGFF.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+# import modules
+import argparse
+
+# create an ArgumentParser object
+parser = argparse.ArgumentParser(description="This script parses a GFF file")
+
+# add positional (required) arguments
+parser.add_argument("gff", help="Name of the GFF file to parse", type=str)
+parser.add_argument("fasta", help="Name of the FASTA file to parse", type=str)
+
+# parse the actual arguments
+# access argument values via `args` variable
+args = parser.parse_args()
+
+# open the GFF file
+with open(args.gff) as x:
+
+	# loop over all the lines in the file
+	for line in x:
+		print(line)
+
+
+
+
+
+
+
+
+
